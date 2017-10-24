7d9b26d4560ed0b4de52f039673b31efb94ed5d8
SaurabhGaur68 <gaursaurabh68@gmail.com>
2017-10-24 17:08:51 +0530

    add android file

---
+++ AlertBoxFirst/app/src/main/java/com/example/gaursaurabh/alertboxfirst/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.gaursaurabh.alertboxfirst;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_alert;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_alert = (Button) findViewById(R.id.button_alert);
+
+        btn_alert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+                a_builder.setMessage("Do you want to close this App!!").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                AlertDialog alert = a_builder.create();
+                alert.setTitle("Alert !!!");
+                alert.show();
+            }
+        });
+    }
+}
